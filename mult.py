ea8ce7684cd6a02ce698cbe7dcaa8281ca8c60bb
Fernando Carvalho <47733765+fndcf@users.noreply.github.com>
2024-08-07 09:38:54 -0300

    Update mult.py
    melhora na escrito do codigo

--- mult.py
+++ mult.py
@@ -6,12 +6,11 @@ first_number = int(input("Enter the first number: "))
 second_number= int(input("Enter the second number: "))
 result = int(first_number * second_number)
 
-if result < 0:
-    print(first_number, "x", second_number, "=", result)
+print(f"{first_number} x {second_number} = {result}")
+
+if result < 0:    
     print("The result is negative.")
 elif result == 0:
-    print(first_number, "x", second_number, "=", result)
     print("The result is positive and negative.")
 else:
-    print(first_number, "x", second_number, "=", result)
     print("The result is positive.")
